3066caa8c4776f69aa392b43a7072b35559317d8
Adim Azbarga <73742109+AdimAzbarga@users.noreply.github.com>
2022-12-13 21:47:56 +0200

    add integration tests

---
+++ UnitTests.py
@@ -0,0 +1,25 @@
+import os
+from kivy.tests.common import GraphicUnitTest
+from kivy.base import EventLoop
+from main import MyApp
+from Converter import ImageConverter
+
+
+class MyTestCase(GraphicUnitTest):
+
+    def test_build(self):
+        window = MyApp()
+        window.run()
+        self.assertNotEqual(window, None)
+
+    def test_size(self):
+        window = MyApp()
+        window.run()
+        self.assertEqual(window.screen.width, 100)
+        self.assertEqual(window.screen.height, 100)
+
+    def test_converter(self):
+        window = MyApp()
+        window.build()
+        ImageConverter(window.DestDir + "/test_pic.png", window.DestDir)
+        self.assertEqual(os.path.exists(window.DestDir + "/surface.stl"), True)
