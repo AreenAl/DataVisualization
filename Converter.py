ac6e9ee0ed9c7a94fef2852ebafd487a37f26961
AreenAl <areenaldda@gmail.com>
2023-04-02 08:35:30 +0300

    convert pdf file to stl file after extracting images

--- Converter.py
+++ Converter.py
@@ -5,6 +5,8 @@ from stl import mesh
 from PIL import Image
 import matplotlib.pyplot as plt
 import pathlib
+from PyPDF2 import PdfReader
+import shutil
 import os
 import fitz
 import cv2
@@ -65,42 +67,17 @@ def ImageConverter(path, destDir):
     iso.save(finalPath)
     print(iso)
 
+def PDFConverter(path,DestDir):
+    reader = PdfReader(path)
+    count = 0
+    for i in range(len(reader.pages)):
+        page = reader.pages[i]
+        for img in page.images:
+            with open(str(count) +img.name, "wb") as fp:
+                fp.write(img.data)
+                count += 1
 
-def PDFConverter(path):
-    # Define path for saved images
-    images_path = 'images/'
-
-    # Open PDF file
-    pdf_file = fitz.open(path)
-
-    # Get the number of pages in PDF file
-    page_nums = len(pdf_file)
-
-    # Create empty list to store images information
-    images_list = []
-
-    # Extract all images information from each page
-    for page_num in range(page_nums):
-        page_content = pdf_file[page_num]
-        images_list.extend(page_content.get_images())
-
-    # Raise error if PDF has no images
-    if len(images_list) == 0:
-        raise ValueError(f'No images found in {path}')
-
-    # Save all the extracted images
-    for i, img in enumerate(images_list, start=1):
-        # Extract the image object number
-        xref = img[0]
-        # Extract image
-        base_image = pdf_file.extract_image(xref)
-        # Store image bytes
-        image_bytes = base_image['image']
-        # Store image extension
-        image_ext = base_image['ext']
-        # Generate image file name
-        image_name = str(i) + '.' + image_ext
-        # Save image
-        with open(os.path.join(images_path, image_name), 'wb') as image_file:
-            image_file.write(image_bytes)
-            image_file.close()
+            Dir=os.path.abspath(os.getcwd())
+            old=os.path.join(Dir, str(count-1) +img.name)
+            finalPath = DestDir + f'/images/'+str(count-1) +img.name
+            shutil.move(old, finalPath)
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from kivy.properties import StringProperty, NumericProperty
 from kivy.uix.popup import Popup
 from kivy.uix.screenmanager import Screen, ScreenManager
 from kivy.uix.widget import Widget
@@ -7,19 +8,27 @@ from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
 from kivymd.uix.screen import MDScreen
 from kivymd.app import MDApp
+from pathlib import Path
 from kivy.uix.image import AsyncImage
-from kivymd.uix.button import MDFlatButton,MDFillRoundFlatButton
+from kivymd.uix.button import MDFlatButton, MDFillRoundFlatButton
 from kivymd.uix.toolbar import MDTopAppBar
 from kivymd.uix.textfield import MDTextField
+from kivymd.uix.list import OneLineListItem, ImageRightWidget
 from plyer import filechooser
 from kivy.lang import Builder
-from Converter import ImageConverter,PDFConverter
-import os,shutil
+
+from Converter import ImageConverter, PDFConverter
+import os, shutil
 
 screen_helper = """
+#:import Label kivy.uix.label.Label
+#:import Image kivy.uix.image.Image
+#:import Button kivy.uix.button.Button
+
 ScreenManager:
     FirstPage:
     SecondPage:
+    ThirdPage:
 <FirstPage>:
     name: 'First'
     MDTopAppBar:
@@ -32,6 +41,7 @@ ScreenManager:
         text_color: 200, 140, 140
         font_size:"25"  
     Button:
+        id: abc
         background_normal: 'upload_b.png'
         pos_hint: {'center_x':0.5,'center_y':0.5}
         size_hint: .3, .3
@@ -40,7 +50,7 @@ ScreenManager:
         text: 'Convert'
         font_size:20
         pos_hint: {'center_x':0.5,'center_y':0.2}
-        on_press: app.btnfunc();root.manager.current = 'Second'
+        on_press: app.btnfunc();root.manager.screens[1].ids.img.source = app.imagepdf
 <SecondPage>:
     name: 'Second'
     MDTopAppBar:
@@ -64,68 +74,121 @@ ScreenManager:
         font_size:20
         pos_hint: {'center_x':0.5,'center_y':0.1}
         on_press: root.manager.current = 'First'
+<ThirdPage>:
+    name: 'Third'
+    BoxLayout:
+        orientation: "vertical"
+        MDTopAppBar:
+            title:"choose image to convert it"
+            pos_hint:{"top":1}
+        ScrollView:
+            size: self.size
+            GridLayout:
+                cols: 2
+                row_force_default: True
+                size_hint_y: None
+                height: self.minimum_height
+                row_default_height: 200
+                top: self.height
+                spacing: "40dp"
+                padding: "70dp"
+                on_kv_post:
+                    [self.add_widget(Button(background_normal=image_path,on_press=lambda button, i=i: app.xxx(i))) for i, image_path in enumerate(app.images)]
+
+
+
+
+
+
 
 """
 
+
 class FirstPage(Screen):
     pass
+
+
 class SecondPage(Screen):
     pass
 
+class ThirdPage(Screen):
+    '''imag = os.listdir('images')
+    for i in range(len(imag)):
+        temp = imag[i]
+        imag[i] = 'images/' + temp
+    '''
+    pass
 # Create the screen manager
 sm = ScreenManager()
 sm.add_widget(FirstPage(name='First'))
 sm.add_widget(SecondPage(name='Second'))
+sm.add_widget(ThirdPage(name='Third'))
+
+
 
 class MyApp(MDApp):
     screen = Screen()
+
     def btnfunc(self):
         print("button is pressed!!")
         print(self.imagePath)
         if '.pdf' in self.imagePath and self.imagePath != '':
-            PDFConverter(self.imagePath)
+            PDFConverter(self.imagePath, self.DestDir)
+            self.root.current = 'Third'
         elif self.imagePath != '':
-            ImageConverter(self.imagePath,self.DestDir)
-
-
+            ImageConverter(self.imagePath, self.DestDir)
+            self.root.current = 'Second'
     def build(self):
         self.imagePath = ''
-        self.imagepdf=''
-        self.temp=''
+        self.imagepdf = ''
+        self.temp = ''
         self.DestDir = os.getcwd()
-        self.theme_cls.primary_palette="Teal"
-        self.toolbar=MDTopAppBar(title="Image to STL")
-        self.toolbar.pos_hint={"top":1}
+        self.theme_cls.primary_palette = "Teal"
+        self.toolbar = MDTopAppBar(title="Image to STL")
+        self.toolbar.pos_hint = {"top": 1}
         self.screen.add_widget(self.toolbar)
+        self.images = os.listdir('images')
+        for i in range(len(self.images)):
+            temp = self.images[i]
+            self.images[i] = 'images/' + temp
         screen = Builder.load_string(screen_helper)
         return screen
+
     def file_chooser(self):
-        file=filechooser.open_file()
+        file = filechooser.open_file()
         self.imagePath = file[0]
         if '.pdf' in self.imagePath and self.imagePath != '':
             self.imagepdf = 'PDF.svg'
         else:
-            self.imagepdf=self.imagePath
+            self.imagepdf = self.imagePath
+
     def directory_chooser(self):
-        file=filechooser.open_file(file_name="surface.stl")
+        file = filechooser.open_file(file_name="surface.stl")
         directoryPath = file[0]
         finalPath = self.DestDir + f'/stlFiles/surface.stl'
         popup = Popup(title='Error message!!',
                       content=MDLabel(text='you must write the name of the file with suffix .stl'
-                                      ,halign='center'),
-                      size_hint=(None, None), size=(450, 150),background='white',title_color='black')
+                                      , halign='center'),
+                      size_hint=(None, None), size=(450, 150), background='white', title_color='black')
         if '.' not in directoryPath:
-            directoryPath +='.stl'
+            directoryPath += '.stl'
         print(directoryPath)
         if '.stl' in directoryPath:
-            self.temp='true'
+            self.temp = 'true'
         else:
-            self.temp='false'
-
-        if self.temp=='true':
+            self.temp = 'false'
+        if self.temp == 'true':
             os.replace(finalPath, directoryPath)
         else:
             popup.open()
+
+    def xxx(self, index):
+        print("Selected button index:", index)
+        self.imagePath=os.path.join(self.DestDir,self.images[index])
+        self.root.screens[0].ids.abc.background_normal = self.images[index]
+        self.imagepdf=self.images[index]
+        print("Directory Path:", os.path.join(self.DestDir,self.images[index]))
+        self.root.current = 'First'
 
 if __name__ == '__main__':
     MyApp().run()
