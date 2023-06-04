26445015118aa729a76e241cc439c442811bb497
Adim Azbarga <73742109+AdimAzbarga@users.noreply.github.com>
2023-06-04 10:39:34 +0300

    improve converter function

--- Converter.py
+++ Converter.py
@@ -44,39 +44,48 @@ def ImageConverter(path, destDir):
     data = np.array(image)
     print('data shape')
     # Create a mesh grid from the data
-    x1, y1 = np.meshgrid(np.arange(data.shape[1]), np.arange(data.shape[0]))
+    x1 = np.arange(data.shape[1])
+    y1 = np.arange(data.shape[0])
+    x1, y1 = np.meshgrid(x1, y1)
     # Normalize the data to a range of 0 to 1 # to increase and decrease the density of the Z index
-    z1 = (data/255)*8
+    z1 = (data/255)*10
 
     grid1 = pv.StructuredGrid(x1, y1, z1)
+    print(grid1)
 
     # Define the dimensions of the grid
     dims = (10, 10, 10)
 
-    # Define the coordinates of the points of the grid
-    print(grid1.dimensions[0])
-    print(grid1.dimensions[1])
     x = np.linspace(-1, grid1.dimensions[1], dims[0])
     y = np.linspace(-1, grid1.dimensions[0], dims[1])
+
     z = np.linspace(-1, 3, dims[2])  # density of the floor
     xx, yy, zz = np.meshgrid(x, y, z, indexing='ij')  # building the floor
     points = np.column_stack((xx.ravel(), yy.ravel(), zz.ravel()))
+
     # Create the structured grid
-    grid = pv.StructuredGrid()
+    grid = pv.StructuredGrid(x, y, z)
     grid.dimensions = dims
     grid.points = points
+    print("printing the floor")
     print(grid)  # bulding the floor
     grid1 = grid1.flip_z()
+    comp = grid.merge(grid1)  # compine the floor and the object
+    #comp = grid
 
-    comp = grid1+grid  # compine the floor and the object
     comp = comp.flip_z()  # to flip the final object for the printer
+    #comp = comp.translate(xyz=(0.240,0.200,1),transform_all_input_vectors=True)
+    comp = comp.scale(xyz=(0.2, 0.2, 0.5),
+                      transform_all_input_vectors=True, inplace=True)
     comp = comp.rotate_x(180)
     iso = comp.extract_geometry()
-    iso.smooth(n_iter=100) # smooth the surface
+    iso.smooth(n_iter=100)
     finalPath = destDir + f'/stlFiles/surface.stl'
     iso.save(finalPath)
     print(iso)
-def PDFConverter(path,DestDir):
+
+
+def PDFConverter(path, DestDir):
     reader = PdfReader(path)
     count = 0
     directory = DestDir + f'/images/'  # directory name
@@ -87,7 +96,7 @@ def PDFConverter(path,DestDir):
     for i in range(len(reader.pages)):
         page = reader.pages[i]
         for img in page.images:
-            with open(os.path.join(DestDir,'images',str(count)+img.name), "wb") as fp:
+            with open(os.path.join(DestDir, 'images', str(count)+img.name), "wb") as fp:
                 fp.write(img.data)
                 count += 1
 
